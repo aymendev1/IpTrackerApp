9a4f3713364093cb8f6238755153ace935f31767
Stephen Ehiabhi <Steveyyh123@gmail.com>
2022-03-11 00:24:36 +0100

    fized the frontend called the backend from the frontned and assigned the data to the frontend wit =h the click of a button 3

--- app.js
+++ app.js
@@ -1,38 +1,39 @@
+const { PORT, APIKEY } = require("./config/config");
 const express = require("express");
-const bodyparser = require("body-parser");
-const request = require("request");
+const fetch = require("node-fetch");
+const cors = require('cors')
 const app = express();
-const https = require("https");
-const http = require("http");
-const { json } = require("express/lib/response");
-
-var UserIP;
-
-const IPURL = "https://api.myip.com";
 
 app.use(express.static("public"));
-app.set("view engine", "pug");
-https.get(IPURL, function (resp) {
-  resp.on("data", function (data) {
-    const info = JSON.parse(data);
-    UserIP = info.ip;
-  });
-});
+app.use(express.json());
+app.use(cors());
+
+//GET route to load the HTML and
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
-  const APIKEY = "f7e3cae938178f7110c6ba666d5037a3";
-  const INFOIPURL =
-    "http://api.ipapi.com/api/" + UserIP + "?access_key=" + APIKEY;
-
-  http.get(INFOIPURL, function (resp) {
-    resp.on("data", function (data) {
-      const info = JSON.parse(data);
-      resp.render(__dirname + "/index.html", { ip: info.ip });
-      console.log(info);
-    });
-  });
 });
 
-app.listen(3000, function () {
-  console.log("serveris running on port 3000");
+//GET route to call the IP apid route and send data to the frontend
+app.get("/ip/info", async (req, res) => {
+  try {
+
+    const INFOIPURL = `https://api.ipgeolocation.io/ipgeo?apiKey=${APIKEY}`
+
+    //fetching the api that gets the all the data about the api
+    const infoData = await fetch(INFOIPURL);
+    const loadedData = await infoData.json();
+
+    //returning the json object
+    return res.status(200).json({ success: true, data: loadedData });
+  } catch (e) {
+    res.status(400).json({ success: false, data: e });
+  }
+});
+
+//server routing port for production or development
+const serverPort = PORT || process.env.PORT;
+
+app.listen(serverPort, (err) => {
+  if (err) console.log("Error connecting to server");
+  console.log(`Server is running on port ${serverPort}`);
 });
